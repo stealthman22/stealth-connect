c5185c492c4cf673a1b823104b7ba4497947af08
stealthman22 <tdotonline@gmail.com>
2020-10-19 20:48:55 +0000

    built custom authentication middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
+const authentication = require('../../middleware/authentication')
+const User = require('../../models/User')
 // should be private
-router.get('/', (req, res) => res.send('Auth route'))
+router.get('/', authentication,  async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).json({msg: 'Server Error'})
+    } 
+})
 
 module.exports = router  
