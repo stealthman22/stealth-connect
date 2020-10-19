353e2dac7f6052a5288dcc7458c917c6b4c7dabb
stealthman22 <tdotonline@gmail.com>
2020-10-19 23:21:54 +0000

    building auth check and login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
 const authentication = require('../../middleware/authentication')
 const User = require('../../models/User')
+const {check, validationResult} = require('express-validator')
+const config = require('../../config/default.json')
+
 // should be private
 router.get('/', authentication,  async (req, res) => {
     try {
@@ -12,5 +16,42 @@ router.get('/', authentication,  async (req, res) => {
         res.status(500).json({msg: 'Server Error'})
     } 
 })
+
+// @route POST api/auth
+// @desc Authenticate and log user in
+// @access Public : 
+router.post('/', [check('email', 'Please enter a valid Email').isEmail(), check('password', 'Password is required').exists()],
+async (req, res) => {
+    // check for erorrs in the body of request
+   const errors = validationResult(req)
+   if(!errors.isEmpty()) {
+       return res.status(400).json({errors: errors.array()})
+   }
+
+   const { email, password} = req.body
+
+try {
+    // check if user exist
+    let user = await User.findOne({email:email})
+    if(user) {
+        return res.status(400).json({errors: [{msg:'Invalid credentials'}]})
+    }
+    // return jwt
+    const payload = await {
+        user: {
+            id: user.id
+        }
+    }
+    // jwt config
+    jwt.sign(payload, config.get('jwtSecret'), {expiresIn:36000}, (err, token) => {
+        if(err) throw err
+    res.json(token)
+    })
+} catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+}
+}
+)
 
 module.exports = router  
--- routes/api/users.js
+++ routes/api/users.js
@@ -9,12 +9,15 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 const {check, validationResult} = require('express-validator')
 // create a route
-// @route GET api/users
+
+// @route POST api/users
 // @desc Register route
 // @access Public : if auth token will be needed or not
+
+
 // validation check is in an array
 // checking if its not there, and not empty at the same time
-router.post('/', [check('name', 'You must enter a Name').not().isEmpty(),check('email', 'Please enter a valid Email').isEmail(), check('password', 'Please enter a Password of not less than 6 characters').isLength({min:6})], 
+router.post('/', [check('name', 'Please enter a valid Name').not().isEmpty(),check('email', 'Please enter a valid Email').isEmail(), check('password', 'Please enter a Password of not less than 6 characters').isLength({min:6})], 
 async (req, res) => {
    const errors = validationResult(req)
    if(!errors.isEmpty()) {
@@ -49,7 +52,7 @@ user = new User({
 const salt = await bcrypt.genSalt(10)
 user.password = await bcrypt.hash(password, salt)
 // save the new user
-user.save()
+await user.save()
 
 // 4 Return jwt 
 const payload = await {
